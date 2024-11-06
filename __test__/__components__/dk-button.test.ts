db1112988d308d125e12d0a313bbbf3229fbeea2
戚永强 <2339208197@qq.com>
2024-11-06 11:03:31 +0800

    test: 💍 完成button单元测试
    完成button单元测试

---
+++ __test__/__components__/dk-button.test.ts
@@ -0,0 +1,138 @@
+import { describe, expect, test } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Button from '../../src/components/Button/Button.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+describe('Compontent-Test:Button.vue', () => {
+    describe('props',()=>{
+        describe('type', () => {
+            test('primary', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        type: 'primary',
+                    },
+                })
+                expect(wrapper.classes()).toContain('dk-button--primary')
+            })
+            test('success', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        type: 'success',
+                    },
+                })
+                expect(wrapper.classes()).toContain('dk-button--success')
+            })
+            test('warning', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        type: 'warning',
+                    },
+                })
+                expect(wrapper.classes()).toContain('dk-button--warning')
+            })
+            test('info', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        type: 'info',
+                    },
+                })
+                expect(wrapper.classes()).toContain('dk-button--info')
+            })
+            test('denger', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        type: 'denger',
+                    },
+                })
+                expect(wrapper.classes()).toContain('dk-button--denger')
+            })
+        })
+    
+        describe('icon', () => {
+            test('button s icon should be user icon', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        icon: 'user',
+                    },
+                    slots: {
+                        default: 'icon button',
+                    },
+                    global: {
+                        stubs: ['FontAwesomeIcon'],
+                    },
+                })
+    
+                const fasIcon = wrapper.findComponent(FontAwesomeIcon)
+                expect(fasIcon.exists()).toBeTruthy()
+                expect(fasIcon.attributes('icon')).toBe('user')
+            })
+        })
+    
+        describe('loading', () => {
+            test('button should dont click and icon is spinner', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        loading: true,
+                    },
+                    slots: {
+                        default: 'loading button',
+                    },
+                    global: {
+                        stubs: ['FontAwesomeIcon'],
+                    },
+                })
+                expect(wrapper.attributes('disabled')).toBeDefined()
+                const fasIcon = wrapper.findComponent(FontAwesomeIcon)
+                expect(fasIcon.exists()).toBeTruthy()
+                expect(fasIcon.attributes('icon')).toBe('spinner')
+            })
+        })
+    
+        describe('disabled', () => {
+            test('button should be disabled', () => {
+                const wrapper = mount(Button, {
+                    props: {
+                        type: 'warning',
+                        disabled: true,
+                    },
+                    slots: {
+                        default: 'attributes button',
+                    },
+                })
+                expect(wrapper.attributes('disabled')).toBeDefined()
+            })
+        })
+    })
+    
+
+    describe('emits', () => {
+        test('button should be clickable', () => {
+            const wrapper = mount(Button, {
+                props: {
+                    type: 'warning',
+                },
+                slots: {
+                    default: 'button to click',
+                },
+            })
+
+            wrapper.get('button').trigger('click')
+            expect(wrapper.emitted()).toHaveProperty('click')
+        })
+
+        test('button didnt click if be disabled', () => {
+            const wrapper = mount(Button, {
+                props: {
+                    type: 'warning',
+                    disabled: true,
+                },
+                slots: {
+                    default: 'disabled button',
+                },
+            })
+            expect(wrapper.attributes('disabled')).toBeDefined()
+            wrapper.get('button').trigger('click')
+            expect(wrapper.emitted()).not.toHaveProperty('click')
+        })
+    })
+})
--- __test__/__example__/function.test.ts
+++ __test__/__example__/function.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test, vi, Mocked } from 'vitest'
-import { tetstFn, testRequest } from './utils/index'
+import { tetstFn, testRequest } from '../utils/index'
 import axios from 'ts-axios-qyq'
 vi.mock('ts-axios-qyq')
 const mockedAxios = axios as Mocked<typeof axios>
@@ -29,14 +29,7 @@ describe('functions', () => {
         expect(spy).toHaveBeenCalledTimes(2)
     })
     test('mock third party module', () => {
-        // mockedAxios.get.mockImplementation(() => {
-        //     return new Promise((resolve) => {
-        //         resolve({ data: 123 } as any)
-        //     })
-        // })
-
         mockedAxios.get.mockResolvedValue({ data: 123 } as any)
-
         testRequest().then((res) => {
             expect(res).toEqual({ data: 123 })
         })
