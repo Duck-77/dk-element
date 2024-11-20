3f503633872226b86559a213195181929c2aea7f
戚永强 <2339208197@qq.com>
2024-11-20 17:36:50 +0800

    docs(component): 📝 修改dk-form 组件文档

--- docs/demo/Form/validate.vue
+++ docs/demo/Form/validate.vue
@@ -5,7 +5,7 @@
             :model="model"
             :rules="rules">
             <FormItem
-                label="username"
+                label="Username"
                 prop="username">
                 <template #label="scope">
                     {{ scope.label }}
@@ -13,18 +13,38 @@
                 <Input v-model="model.username" />
             </FormItem>
             <FormItem
-                label="password"
+                label="Password"
                 prop="password">
                 <Input
                     type="password"
                     v-model="model.password" />
             </FormItem>
             <FormItem
-                label="email"
+                label="Email"
                 prop="email">
                 <Input
                     type="email"
                     v-model="model.email" />
+            </FormItem>
+            <FormItem
+                label="Gender"
+                prop="gender">
+                <RadioGroup v-model="model.gender">
+                    <Radio value="1">男</Radio>
+                    <Radio value="0">女</Radio>
+                </RadioGroup>
+            </FormItem>
+            <FormItem
+                label="Adult"
+                prop="adult">
+                <Switch v-model="model.adult"> </Switch>
+            </FormItem>
+            <FormItem
+                label="Address"
+                prop="address">
+                <Select
+                    :options="Address"
+                    v-model="model.address"></Select>
             </FormItem>
         </Form>
         <div>
@@ -42,6 +62,10 @@ import Form from '@/components/Form/src/Form.vue'
 import FormItem from '@/components/Form/src/FormItem.vue'
 import Input from '@/components/Input/Input.vue'
 import Button from '@/components/Button/Button.vue'
+import Select from '@/components/Select/Select.vue'
+import Radio from '@/components/Radio/src/Radio.vue'
+import RadioGroup from '@/components/Radio/src/RadioGroup.vue'
+import Switch from '@/components/Switch/Switch.vue'
 import { reactive, ref } from 'vue'
 
 const formRef = ref()
@@ -50,13 +74,30 @@ const model = reactive({
     username: '',
     password: '',
     email: '',
+    address: '',
+    gender: '',
+    adult: false,
 })
 
 const rules = {
     username: [{ type: 'string', trigger: 'input', max: 5, min: 3 }],
     password: [{ type: 'string', required: true, trigger: 'blur' }],
     email: [{ type: 'email', required: true, trigger: 'blur' }],
+    address: [
+        { required: true, trigger: 'blur' },
+        { type: 'string', required: true, trigger: 'change' },
+    ],
+    gender: [{ type: 'string', required: true, trigger: 'change' }],
+    adult: [{ type: 'boolean', trigger: 'change' }],
 }
+const Address = [
+    {
+        label: 'a',
+        value: 'a',
+    },
+    { label: 'b', value: 'b' },
+    { label: 'c', value: 'c' },
+]
 
 const submit = async () => {
     try {
